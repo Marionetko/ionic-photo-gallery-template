9ef54c4b759d0130076204a7cd0a0f58fd57f21e
Marionetko <webius.net@gmail.com>
2023-01-17 14:16:26 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic-photo-gallery-template',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
