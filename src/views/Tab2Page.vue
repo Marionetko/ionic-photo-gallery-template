145ae49b4e7ebd7507c080d1fe83e160bd871ed4
Marionetko <webius.net@gmail.com>
2023-01-17 14:48:22 +0200

    added the FAB

--- src/views/Tab2Page.vue
+++ src/views/Tab2Page.vue
@@ -6,12 +6,11 @@
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Tab 2</ion-title>
-        </ion-toolbar>
-      </ion-header>
-
+      <ion-fab vertical="bottom" horizontal="center" slot="fixed">
+        <ion-fab-button @click="takePhoto()">
+          <ion-icon :icon="camera"></ion-icon>
+        </ion-fab-button>
+      </ion-fab>
     </ion-content>
   </ion-page>
 </template>
