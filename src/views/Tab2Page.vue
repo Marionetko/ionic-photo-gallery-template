f70819dee3f3eb9a8935705a6fe31b4a266c6463
Marionetko <webius.net@gmail.com>
2023-01-17 18:17:56 +0200

    added delete func

--- src/views/Tab2Page.vue
+++ src/views/Tab2Page.vue
@@ -9,7 +9,7 @@
       <ion-grid>
         <ion-row>
           <ion-col size="6" :key="photo" v-for="photo in photos">
-            <ion-img :src="photo.webviewPath"></ion-img>
+            <ion-img :src="photo.webviewPath" @click="showActionSheet(photo)"></ion-img>
           </ion-col>
         </ion-row>
       </ion-grid>
@@ -23,21 +23,74 @@
 </template>
 
 <script setup lang="ts">
+import { camera, trash, close } from 'ionicons/icons';
 import {
-  IonContent,
-  IonCol,
-  IonFab,
-  IonFabButton,
-  IonGrid,
+  actionSheetController,
   IonPage,
   IonHeader,
+  IonFab,
+  IonFabButton,
   IonIcon,
-  IonImg,
-  IonRow,
-  IonTitle,
   IonToolbar,
+  IonTitle,
+  IonContent,
+  IonImg,
+  IonGrid,
+  IonRow,
+  IonCol,
 } from '@ionic/vue';
-import { camera, trash, close } from 'ionicons/icons';
 import { usePhotoGallery, UserPhoto } from '@/composables/usePhotoGallery';
-const { photos, takePhoto } = usePhotoGallery();
+import { defineComponent} from 'vue';
+export default defineComponent({
+  name: 'Tab2',
+  components: {
+    IonHeader,
+    IonFab,
+    IonIcon,
+    IonFabButton,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonPage,
+    IonGrid,
+    IonRow,
+    IonCol,
+    IonImg,
+  },
+  setup() {
+    const { photos, takePhoto, deletePhoto } = usePhotoGallery();
+    const showActionSheet = async (photo: UserPhoto) => {
+      const actionSheet = await actionSheetController.create({
+        header: 'Photos',
+        buttons: [
+          {
+            text: 'Delete',
+            role: 'destructive',
+            icon: trash,
+            handler: () => {
+              deletePhoto(photo);
+            },
+          },
+          {
+            text: 'Cancel',
+            icon: close,
+            role: 'cancel',
+            handler: () => {
+              // Nothing to do, action sheet is automatically closed
+            },
+          },
+        ],
+      });
+      await actionSheet.present();
+    };
+    return {
+      photos,
+      takePhoto,
+      showActionSheet,
+      camera,
+      trash,
+      close,
+    };
+  },
+});
 </script>
